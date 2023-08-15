de1f7afd11f5166258e11dc575dcc1f342ce9288
K.Shravya Shetty <21j14.shravya@sjec.ac.in>
2023-08-15 21:38:34 +0930

    comments

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php tutorial</title>
+</head>
+<body>
+    <div class="container">
+        this is my first php file
+        <?php
+        echo "hello world this is printed using php";
+        //this is a comment
+        /*this is 
+        a multiline commemt*/
+        ?>
+    </div>
+</body>
+</html>
