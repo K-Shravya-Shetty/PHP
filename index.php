35b665ac4e55180f1f9dc410d77d18e2eeca7363
K.Shravya Shetty <21j14.shravya@sjec.ac.in>
2023-08-24 23:03:54 +0930

    connection done

--- index.php
+++ index.php
@@ -7,13 +7,5 @@
     if(!$con){
         die("connect to this database failed due to".mysqli_connect_error());
     }
-    //echo "successfully connected"
-    $name=$_POST['name'];
-    $gender=$_POST['gender'];
-    $age=$_POST['age'];
-    $email=$_POST['email'];
-    $phone=$_POST['phone'];
-    $descr=$_POST['descr'];
-    $sql="INSERT INTO `trip` (`sno`, `name`, `age`, `gender`, `email`, `phone`, `descr`, `dt`) VALUES ( '$name', '$age', '$gender', '$email', '$phone', '$descr', current_timestamp());";
-    echo $sql;
+    echo "successfully connected"
 ?>
